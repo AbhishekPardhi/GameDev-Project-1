c36e901f4bd7442df52eb86af694ab123e7a8749
Soham Samaddar <sohams20@iitk.ac.in>
2021-10-02 22:13:57 +0530

    Background Manager

--- Monkey KickOff/Assets/Scripts/BackgroundManager.cs
+++ Monkey KickOff/Assets/Scripts/BackgroundManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BackgroundManager : MonoBehaviour
 {
@@ -8,38 +9,62 @@ public class BackgroundManager : MonoBehaviour
     public GameObject bg1;
     public GameObject bg2;
     public GameObject bg3;
+    public GameObject bg4;
+    public GameObject bg5;
+    
     public Sprite map1;
     public Sprite map2;
     public Sprite map3;
     public Sprite map4;
-    public Transform ball;
+
+    public Sprite forest;
+    public Sprite desert;
+    public Sprite city;
+    public Sprite space;
+
+    public Rigidbody2D ball;
     public Camera cam;
+
+    private float offset = 10.66f;
+    private float relVel = 0.0f;
+    private Queue<GameObject> backgrounds = new Queue<GameObject>();
+
     void Start()
     {
-        SetBg(bg1, map1);
-        SetBg(bg2, map1);
-        SetBg(bg3, map1);
+        SetBg(bg1, forest);
+        SetBg(bg2, forest);
+        SetBg(bg3, forest);
+        SetBg(bg4, forest);
+        SetBg(bg5, forest);
         cam = FindObjectOfType<Camera>();
+        backgrounds.Enqueue(bg1);
+        backgrounds.Enqueue(bg2);
+        backgrounds.Enqueue(bg3);
+        backgrounds.Enqueue(bg4);
+        backgrounds.Enqueue(bg5);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if(cam.transform.position.x>(bg2.transform.position.x+ bg3.transform.position.x)/2)
+        relVel = ball.velocity.x/2;
+        foreach(GameObject bg in backgrounds)
         {
-            bg1.transform.position += new Vector3(10, 0, 0);
-            bg2.transform.position += new Vector3(10, 0, 0);
-            bg3.transform.position += new Vector3(10, 0, 0);
+            bg.transform.position += Vector3.right * (ball.velocity.x - relVel) * Time.fixedDeltaTime;
         }
-        if (cam.transform.position.x > 85) SetBg(bg3, map2);
-        if (cam.transform.position.x > 95) SetBg(bg2, map2);
-        if (cam.transform.position.x > 105) SetBg(bg1, map2);
-        if (cam.transform.position.x > 185) SetBg(bg3, map3);
-        if (cam.transform.position.x > 195) SetBg(bg2, map3);
-        if (cam.transform.position.x > 205) SetBg(bg1, map3);
-        if (cam.transform.position.x > 285) SetBg(bg3, map4);
-        if (cam.transform.position.x > 295) SetBg(bg2, map4);
-        if (cam.transform.position.x > 305) SetBg(bg1, map4);
+
+        if(cam.transform.position.x - backgrounds.Peek().transform.position.x > 5*offset/2) {
+            GameObject temp = backgrounds.Dequeue();
+            if(cam.transform.position.x > 100)
+                SetBg(temp, desert);
+            if(cam.transform.position.x > 200)
+                SetBg(temp, city);
+            if(cam.transform.position.x > 300)
+                SetBg(temp, space);
+            temp.transform.position += new Vector3(4*offset, 0, 0);
+            backgrounds.Enqueue(temp);
+        }
+        
     }
     void SetBg(GameObject bg, Sprite map)
     {
